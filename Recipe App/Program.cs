3fb98b50f751c04af95e9f5adc2c7b2f55a89116
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-04-11 21:01:42 +0200

    Imported the neccesary libraries and created the recipe class as well as creating the needed arraylists

--- Recipe App/Program.cs
+++ Recipe App/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace Recipe_App
+﻿// Importing the necessary libraries.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Recipe_App
 {
     internal class Program
     {
@@ -6,5 +15,18 @@
         {
             Console.WriteLine("Hello, World!");
         }
+    }
+
+    /// Represents a recipe with ingredients and steps.
+
+    public class Recipe
+    {
+
+        // ArrayLists for storing the recipe information.
+        public ArrayList ingredientsName = new ArrayList();
+        public ArrayList ingredientsAmount = new ArrayList();
+        public ArrayList ingredientsUnit = new ArrayList();
+        public ArrayList steps = new ArrayList();
+        public ArrayList originalQuantities = new ArrayList();
     }
 }
