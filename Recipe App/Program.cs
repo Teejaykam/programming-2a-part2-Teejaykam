eed36f2e798118da9b3203195db495396e3b2a89
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-04-11 21:22:29 +0200

    Finished Creating the method within the recipe class, allowing the user to input the ingredient info to be stored in an arraylist

--- Recipe App/Program.cs
+++ Recipe App/Program.cs
@@ -28,5 +28,24 @@ namespace Recipe_App
         public ArrayList ingredientsUnit = new ArrayList();
         public ArrayList steps = new ArrayList();
         public ArrayList originalQuantities = new ArrayList();
+
+        // Constructor that allows the user to add ingredients: name, quantity, and unit of measurement.
+        public void addIngredient()
+        {
+            // Prompt the user to enter the name of the ingredient
+            Console.WriteLine("Enter the name of the ingredient: ");
+            ingredientsName.Add(Console.ReadLine());
+
+            // Prompt the user to enter the quantity of the ingredient
+            Console.WriteLine("Enter the quantity of the ingredient: ");
+            double quantity = Convert.ToDouble(Console.ReadLine());
+            ingredientsAmount.Add(quantity);
+            originalQuantities.Add(quantity);
+
+            // Prompt the user to enter the unit of measurement for the ingredient
+            Console.WriteLine("Enter the unit of measurement: ");
+            ingredientsUnit.Add(Console.ReadLine());
+        }
+
     }
 }
