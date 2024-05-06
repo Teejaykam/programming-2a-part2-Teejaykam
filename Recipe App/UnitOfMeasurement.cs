8d17e3f8dd041fbc0f8ae6f818402b0084c17470
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-06 22:24:17 +0200

    Create an enum containing the values for each measurement and the methods to convert them to their full names, singular and plural

--- Recipe App/UnitOfMeasurement.cs
+++ Recipe App/UnitOfMeasurement.cs
@@ -1,8 +1,67 @@
 ﻿using System;
 
-public class UnitOfMeasurement
+namespace Recipe_App
 {
-	public Class1()
-	{
-	}
+    internal enum Units
+    {
+        None = 0,
+        Tsp = 1,
+        Tbsp = 2,
+        Cup = 3,
+        G = 4,
+        Kg = 5,
+        Ml = 6,
+        L = 7
+    }
+
+    internal class UnitOfMeasurement
+    {
+        public static string UnitsToNameS(Units UoM)
+        {
+            switch(UoM)
+            {
+                case Units.None:
+                    return null;  
+                case Units.Tsp:
+                    return "tea spoon";   
+                case Units.Tbsp:
+                    return "table spoon";  
+                case Units.Cup:
+                    return "cup";   
+                case Units.G:
+                    return "gram"; 
+                case Units.Kg:
+                    return "kilogram";
+                case Units.Ml:
+                    return "millilitre";    
+                case Units.L:
+                    return "litre";
+            }
+            return null;
+        }
+
+        public static string UnitsToNameM(Units UoM)
+        {
+            switch (UoM)
+            {
+                case Units.None:
+                    return null;
+                case Units.Tsp:
+                    return "tea spoons";
+                case Units.Tbsp:
+                    return "table spoons";
+                case Units.Cup:
+                    return "cups";
+                case Units.G:
+                    return "grams";
+                case Units.Kg:
+                    return "kilograms";
+                case Units.Ml:
+                    return "millilitres";
+                case Units.L:
+                    return "litres";
+            }
+            return null;
+        }
+    }
 }
