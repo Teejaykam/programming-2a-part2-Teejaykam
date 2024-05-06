fc1a49ec171ab5155d736f70aea3d15852b61e52
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-06 23:00:41 +0200

    Created the file and methods for customizing the console using a single method called setcolor. it includes a dictionary that contains all the colours.

--- Recipe App/ConsoleCustomization.cs
+++ Recipe App/ConsoleCustomization.cs
@@ -1,8 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class ConsoleCustomization
+namespace Recipe_App
 {
-	public Class1()
-	{
-	}
-}
+    public enum Colours
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Cyan,
+        White,
+        Black
+    }
+
+    internal class ConsoleCustomization
+    {
+        private static readonly Dictionary<Colours, ConsoleColor> ColorMap = new Dictionary<Colours, ConsoleColor>
+        {
+            { Colours.Red, ConsoleColor.Red },
+            { Colours.Green, ConsoleColor.Green },
+            { Colours.Blue, ConsoleColor.Blue },
+            { Colours.Yellow, ConsoleColor.Yellow },
+            { Colours.Cyan, ConsoleColor.Cyan },
+            { Colours.White, ConsoleColor.White },
+            { Colours.Black, ConsoleColor.Black }
+        };
+
+        public static void SetColor(string text, Colours bgColor, Colours fgColor, bool newLine, bool reset)
+        {
+            ConsoleColor bgConsoleColor;
+            if (ColorMap.TryGetValue(bgColor, out bgConsoleColor))
+            {
+                Console.BackgroundColor = bgConsoleColor;
+            }
+            else
+            {
+                Console.ResetColor();
+            }
+
+            ConsoleColor fgConsoleColor;
+            if (ColorMap.TryGetValue(fgColor, out fgConsoleColor))
+            {
+                Console.ForegroundColor = fgConsoleColor;
+            }
+            else
+            {
+                Console.ResetColor();
+            }
+
+            if (newLine)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.Write(text);
+            }
+
+            if (reset)
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+} 
