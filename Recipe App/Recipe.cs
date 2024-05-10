5ed445c3f62354a5108b9982838e29c9ee6009ae
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-10 22:36:14 +0200

    created he printrecipe method

--- Recipe App/Recipe.cs
+++ Recipe App/Recipe.cs
@@ -1,8 +1,55 @@
 ﻿using System;
 
-public class Recipe
+namespace Recipe_App
 {
-	public recipe()
-	{
-	}
+    internal static class Recipes
+    {
+        public static void PrintRecipe(List<string> ingredientsName, List<double> ingredientsAmount, List<string> ingredientsUnit, List<string> steps)
+        {
+            // Check if there are any ingredients in the recipe
+            if (ingredientsName.Count == 0)
+            {
+                Console.WriteLine("There are no ingredients in the recipe.");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("============================================================================");
+                Console.ResetColor();
+                return;
+            }
+            else
+            {
+                // Print the ingredients
+                Console.WriteLine("Ingredients: ");
+                // Loop through the List and print the ingredients
+                for (int i = 0; i < ingredientsName.Count; i++)
+                {
+                    // Print the ingredients
+                    Console.WriteLine(ingredientsAmount[i] + " " + ingredientsUnit[i] + " of " + ingredientsName[i]);
+                }
+            }
+            // Check if there are any steps in the recipe
+            if (steps.Count == 0)
+            {
+                Console.WriteLine("There are no steps in the recipe.");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("============================================================================");
+                Console.ResetColor();
+                return;
+            }
+            else
+            {
+                // Print the steps
+                Console.WriteLine("\nSteps: ");
+                // Loop through the List and print the steps
+                for (int i = 0; i < steps.Count; i++)
+                {
+                    // Print the steps
+                    Console.WriteLine((i + 1) + ". " + steps[i]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("============================================================================");
+            Console.ResetColor();
+        }
+    }
 }
+
