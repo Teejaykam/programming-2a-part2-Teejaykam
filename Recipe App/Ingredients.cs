446c31743b169986659fe1e878b8a6b5ff8c4532
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-11 17:16:55 +0200

    Created a new ingredients class for easier management of the ingredients

---
+++ Recipe App/Ingredients.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Recipe_App
+{
+    public class Ingredients
+    {
+        public string Name { get; set; }
+        public string Measurement { get; set; }
+        public float Quantity { get; set; }
+        public float Calories { get; set; }
+        public string FoodGroup { get; set; }
+        public float OriginalQuantity { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ingredients"/> class.
+        /// </summary>
+        /// <param name="name">The name of the ingredient.</param>
+        /// <param name="quantity">The quantity of the ingredient.</param>
+        /// <param name="originalQuantity">The original quantity of the ingredient.</param>
+        /// <param name="calories">The number of calories in the ingredient.</param>
+        /// <param name="group">The food group of the ingredient.</param>
+        /// <param name="measurement">The measurement of the ingredient.</param>
+        public Ingredients(string name, float quantity, float originalQuantity, float calories, string group, string measurement)
+        {
+            Name = name;
+            Quantity = quantity;
+            OriginalQuantity = originalQuantity;
+            Calories = calories;
+            FoodGroup = group;
+            Measurement = measurement;
+        }
+    }
+}
