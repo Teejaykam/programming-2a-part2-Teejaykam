3e248fab934b73bcfb319d0243fb2e91f8868c5c
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-12 21:51:55 +0200

    Updated the ConvertUnits method which scales the recipe

--- Recipe App/EditQuantities.cs
+++ Recipe App/EditQuantities.cs
@@ -1,134 +1,210 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
+
 
 namespace Recipe_App
 {
     public class EditQuantities
     {
         // Method for scaling
-        public static void ConvertUnits(List<string> ingredientsList, List<double> originalQuantities)
+        public static void ConvertUnits(List<Recipe> recipes)
         {
             // Check if there are any ingredients in the recipe
-            if (ingredientsList.Count <= 0)
+            if (recipes.Count <= 0)
             {
-                Console.WriteLine("There are no ingredients in the recipe.");
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("============================================================================");
-                Console.ResetColor();
+                ConsoleCustomization.SetColor("There are no recipes.", ConsoleCustomization.Colours.Red,
+                    ConsoleCustomization.Colours.White, true, false);
+                ;
+                Program.Menu();
                 return;
             }
 
-            else
-            {
-                double factor = 1;
-                // Prompt the user to enter the unit of measurement they want to convert to
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("See the recipe at a different scale: ");
-                Console.ResetColor();
-                Console.WriteLine("Enter the number corresponding with the scale value you require: \n" +
-                    "1: A factor of 0.5 (half)\n" +
-                    "2: A factor of 2 (double)\n" +
-                    "3: A factor of 3 (triple)\n" +
-                    "4: Return to the main menu");
-                int option = Convert.ToInt32(Console.ReadLine());
-                // Set the factor based on the user's choice
-                switch (option)
-                {
-                    case 1:
-                        factor = 0.5;
-                        break;
-                    case 2:
-                        factor = 2;
-                        break;
-                    case 3:
-                        factor = 3;
-                        break;
-                    case 4:
-                        Console.Clear();
-                        return;
-                    default:
-                        Console.WriteLine("Invalid option. Please try again.");
-                        break;
-                }
+            Console.WriteLine("Select a recipe to scale its quantities: ");
+            int choice = 0;
 
-                // Convert the units
-                if (originalQuantities.Count <= 0)
+            foreach (Recipe recipe in recipes)
+            {
+                ConsoleCustomization.SetColor(
+                    $"\n{++choice}. Recipe Name: {recipe.GetName()} | (Calories: {HelperClass.CalculateTotalCalories(recipe)})",
+                    ConsoleCustomization.Colours.Black, ConsoleCustomization.Colours.White, true, true);
+                Console.WriteLine("List of Ingredients: ");
+                List<Ingredients> ingredients = recipe.GetIngredients();
+
+                if (ingredients.Count <= 0)
                 {
-                    Console.WriteLine("There are no ingredients to scale.");
-                    return;
+                    Console.WriteLine("There are no ingredients in the recipe.");
                 }
                 else
                 {
-                    // Loop through the List and scale the quantities
-                    for (int i = 0; i < originalQuantities.Count; i++)
+                    foreach (Ingredients ingredient in ingredients)
                     {
-                        // Scale the quantities
-                        double scaledQuantity = originalQuantities[i] * factor;
-                        originalQuantities[i] = scaledQuantity;
+                        ConsoleCustomization.SetColor(
+                            $"\n- Name: {ingredient.Name}" +
+                            $"\n- Quantity: {ingredient.Quantity}" +
+                            $"\n- Measurement: {ingredient.Measurement}" +
+                            $"\n- Calories: {ingredient.Calories}" +
+                            $"\n- Food Group: {ingredient.FoodGroup}",
+                            ConsoleCustomization.Colours.Black,
+                            ConsoleCustomization.Colours.White,
+                            true,
+                            false
+                        );
                     }
                 }
+            }
+            Console.Write("Enter your choice: ");
+            string? decision = Console.ReadLine();
 
-                Console.WriteLine("The recipe has been scaled: ");
-                PrintRecipe();
-                Console.WriteLine("Do you wish to reset the quantities to their original values? (y/n) ");
-                string answer = Console.ReadLine();
-                if (answer == "y")
+            while (decision == null || !HelperClass.ValidInteger(decision) || int.Parse(decision) > recipes.Count ||
+                   int.Parse(decision) < 1)
+            {
+                Console.Write($"Enter a valid recipe number between 1 and {recipes.Count}: ");
+                decision = Console.ReadLine();
+            }
+
+            int ans = int.Parse(decision);
+            int count = 0;
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (count == ans - 1)
                 {
-                    ResetQuantities(ingredientsList, originalQuantities);
+                    List<Ingredients> ingredients = recipe.GetIngredients();
+
+                    Console.Write("Enter a valid scaling factor: ");
+                    string factorStr = Console.ReadLine();
+
+                    while(factorStr == null || !HelperClass.ValidFloat((factorStr)) || float.Parse(factorStr) > 50.00)
+                    {
+                        Console.Write("Enter a valid scaling factor between 0 and 50: ");
+                        factorStr = Console.ReadLine();
+                    }
+
+                    float factor = float.Parse(factorStr);
+
+                    int counter = 0;
+
+                    foreach (Ingredients ingredient in ingredients)
+                    {
+                        ingredient.Quantity *= factor;
+                        ingredient.Calories *= factor;
+                        counter++;
+                    }
+                } else if (count > ans)
+                {
+                    break;
+                }
+
+                count++;
+            }
+            Console.WriteLine("The recipe has been scaled: ");
+            Program.Menu();
+        }
+
+        public static void ResetQuantities(List<Recipe> recipes)
+        {
+            if (recipes.Count <= 0)
+            {
+                ConsoleCustomization.SetColor("There are no recipes.", ConsoleCustomization.Colours.Red,
+                    ConsoleCustomization.Colours.White, true, false);
+                ;
+            }
+
+            ConsoleCustomization.SetColor("Select a recipe to reset its quantity", ConsoleCustomization.Colours.Cyan,
+                ConsoleCustomization.Colours.White, true, false);
+            int choice = 0;
+
+            foreach (Recipe recipe in recipes)
+            {
+                ConsoleCustomization.SetColor(
+                    $"\n{++choice}. Recipe Name: {recipe.GetName()} | (Calories: {HelperClass.CalculateTotalCalories(recipe)})",
+                    ConsoleCustomization.Colours.Black, ConsoleCustomization.Colours.White, true, true);
+                Console.WriteLine("List of Ingredients: ");
+                List<Ingredients> ingredients = recipe.GetIngredients();
+
+                if (ingredients.Count <= 0)
+                {
+                    Console.WriteLine("There are no ingredients in the recipe.");
+                }
+                else
+                {
+                    foreach (Ingredients ingredient in ingredients)
+                    {
+                        ConsoleCustomization.SetColor(
+                            $"\n- Name: {ingredient.Name}" +
+                            $"\n- Quantity: {ingredient.Quantity}" +
+                            $"\n- Measurement: {ingredient.Measurement}" +
+                            $"\n- Calories: {ingredient.Calories}" +
+                            $"\n- Food Group: {ingredient.FoodGroup}",
+                            ConsoleCustomization.Colours.Black,
+                            ConsoleCustomization.Colours.White,
+                            true,
+                            false
+                        );
+                    }
                 }
             }
-        }
-        public static void ResetQuantities(List<string> ingredientsAmount, List<double> originalQuantities)
-        {
-            ConsoleCustomization.SetColor("There are no ingredients in the recipe.", ConsoleCustomization.Colours.Red, ConsoleCustomization.Colours.White, true, false);
-            if (ingredientsAmount.Count <= 0)
+
+            ConsoleCustomization.SetColor("Enter your choice: ", ConsoleCustomization.Colours.Yellow,
+                ConsoleCustomization.Colours.White, false, false);
+            string choiceAnswer = Console.ReadLine();
+
+            while (!HelperClass.ValidInteger(choiceAnswer) || int.Parse(choiceAnswer) > recipes.Count ||
+                   int.Parse(choiceStr) < 1)
             {
-                return;
+                Console.WriteLine($"Enter a valid number between 1 and {recipes.Count}: ");
+                choiceAnswer = Console.ReadLine();
             }
 
-            ConsoleCustomization.SetColor("Are you sure you want to reset the quantities? (y/n) ", ConsoleCustomization.Colours.Yellow, ConsoleCustomization.Colours.White, false, false);
+            int option = int.Parse(choiceAnswer);
+
+            ConsoleCustomization.SetColor("Are you sure you want to reset the quantities? (y/n) ",
+                ConsoleCustomization.Colours.Yellow, ConsoleCustomization.Colours.White, false, false);
             string answer = Console.ReadLine();
             if (answer == "y")
             {
-                ConsoleCustomization.SetColor("The recipe has been reset: ", ConsoleCustomization.Colours.Red, ConsoleCustomization.Colours.White, true, false);
+                ConsoleCustomization.SetColor("The recipe has been reset: ", ConsoleCustomization.Colours.Red,
+                    ConsoleCustomization.Colours.White, true, false);
                 ingredientsAmount.Clear();
                 ingredientsAmount.AddRange(originalQuantities);
-                ConsoleCustomization.SetColor("The Original Recipe: ", ConsoleCustomization.Colours.Red, ConsoleCustomization.Colours.White, true, false);
-                PrintRecipe(ingredientsAmount);
+                ConsoleCustomization.SetColor("The Original Recipe: ", ConsoleCustomization.Colours.Red,
+                    ConsoleCustomization.Colours.White, true, false);
+                PrintRecipe(recipe);
             }
         }
 
-        private void PrintRecipe(IEnumerable<double> ingredientsAmount)
-        {
-            foreach (var ingredient in ingredientsAmount)
-            {
-                Console.WriteLine(ingredient);
-            }
-        }
 
         // Clear the data
-        public static void ClearData(List<string> ingredientsName, List<double> ingredientsAmount, List<string> ingredientsUnit, List<string> steps, List<double> originalQuantities>)
+        public static void ClearData(List<Recipe> recipes)
         {
-            Console.WriteLine("Are you sure you want to clear the data? (y/n) ");
+            Console.WriteLine("Are you sure you want to clear the data?\n1.Yes\n2.No");
+            Console.Write("Enter your choice: ");
             string answer = Console.ReadLine();
-            if (answer == "y")
+            while (!HelperClass.ValidInteger(answer))
             {
+                Console.Write("Please enter a valid option: ");
+                answer = Console.ReadLine();
+            }
+
+            int.TryParse(answer, out int validAnswer);
+
+            if (validAnswer == 1)
+            {
+                // Clearing the data in the Lists
+                recipes.Clear();
                 // Clear the data
                 Console.WriteLine("The data has been cleared.");
                 Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("============================================================================");
+                Console.WriteLine(
+                    "============================================================================\nPress any key to continue...");
                 Console.ResetColor();
-                // Clearing the data in the Lists
-                ingredientsName.Clear();
-                ingredientsAmount.Clear();
-                ingredientsUnit.Clear();
-                steps.Clear();
-                originalQuantities.Clear();
+                Console.ReadKey();
             }
             else
             {
-                return;
+                Program.Menu();
             }
         }
     }
-} 
+}
