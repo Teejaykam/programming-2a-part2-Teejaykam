f1890e19adcccd19de30c73d9eb03fbcd43b26c6
Teejay Kamwaro <130388973+Teejaykam@users.noreply.github.com>
2024-05-29 20:02:55 +0200

    Created the unit tests for testing the total calorie calculation

---
+++ Recipe App/CalorieCalculationTest.cs
@@ -0,0 +1,27 @@
+﻿using Recipe_App;
+
+namespace Recipe_App
+{
+    public class CalorieCalculationTest
+    {
+        public static void RunTest()
+        {
+            Recipe testRecipe = new Recipe("Test Recipe");
+            testRecipe.AddIngredient(new Ingredients("Test Ingredient 1", 100, 100, 50, "Grains","grams"));
+            testRecipe.AddIngredient(new Ingredients("Test Ingredient 2", 200, 200, 100, "Protein","grams"));
+            testRecipe.AddIngredient(new Ingredients("Test Ingredient 3", 300, 300, 120, "Vegetables","grams"));
+
+            float expectedCalories = 270;
+            float actualCalories = HelperClass.CalculateTotalCalories(testRecipe);
+
+            if (expectedCalories == actualCalories)
+            {
+                ConsoleCustomization.SetColor("Test Passed: The total calories were calculated correctly.", ConsoleCustomization.Colours.Black, ConsoleCustomization.Colours.Green, true, true);
+            }
+            else
+            {
+                ConsoleCustomization.SetColor("Test Failed: The total calories were not calculated correctly.", ConsoleCustomization.Colours.Black, ConsoleCustomization.Colours.Red, true, true);
+            }
+        }
+    }
+}
--- Recipe App/RecipeUnitTests.cs
+++ Recipe App/RecipeUnitTests.cs
@@ -1,8 +1,73 @@
-﻿using System;
+﻿using Recipe_App;
+using NUnit.Framework;
 
-public class RecipeUnitTests
+[Test]
+public void CalculateTotalCalories_ReturnsZero_WhenRecipeHasNoIngredients()
 {
-	public Class1()
-	{
-	}
+    Recipe recipe = new Recipe();
+    Assert.AreEqual(0, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_ReturnsSumOfIngredientCalories_WhenRecipeHasIngredients()
+{
+    Recipe recipe = new Recipe();
+    recipe.AddIngredient(new Ingredients { Calories = 10 });
+    recipe.AddIngredient(new Ingredients { Calories = 20 });
+    Assert.AreEqual(30, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_HandlesNullIngredients()
+{
+    Recipe recipe = new Recipe();
+    recipe.AddIngredient(null);
+    recipe.AddIngredient(new Ingredients { Calories = 10 });
+    Assert.AreEqual(10, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_ReturnsZero_WhenRecipeHasNullIngredients()
+{
+    Recipe recipe = new Recipe();
+    recipe.Ingredients = null;
+    Assert.AreEqual(0, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_ReturnsZero_WhenRecipeHasEmptyIngredientsList()
+{
+    Recipe recipe = new Recipe();
+    recipe.Ingredients = new List<Ingredients>();
+    Assert.AreEqual(0, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_ReturnsSumOfIngredientCalories_WhenIngredientsHaveNegativeCalories()
+{
+    Recipe recipe = new Recipe();
+    recipe.AddIngredient(new Ingredients { Calories = -10 });
+    recipe.AddIngredient(new Ingredients { Calories = -20 });
+    Assert.AreEqual(-30, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_ReturnsSumOfIngredientCalories_WhenIngredientsHaveMixedCalories()
+{
+    Recipe recipe = new Recipe();
+    recipe.AddIngredient(new Ingredients { Calories = 10 });
+    recipe.AddIngredient(new Ingredients { Calories = -20 });
+    Assert.AreEqual(-10, RecipeCalculator.CalculateTotalCalories(recipe));
+}
+
+[Test]
+public void CalculateTotalCalories_CallsDelegate_WhenTotalCaloriesExceeds300()
+{
+    Recipe recipe = new Recipe();
+    recipe.AddIngredient(new Ingredients { Calories = 250 });
+    recipe.AddIngredient(new Ingredients { Calories = 50 });
+    bool delegateCalled = false;
+    recipe.CaloriesExceeded += (totalCalories) => delegateCalled = true;
+    RecipeCalculator.CalculateTotalCalories(recipe);
+    Assert.IsTrue(delegateCalled);
 }
